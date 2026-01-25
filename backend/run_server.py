ed5f3ed248b0a329a558cb36e7682436785e541f
Oleksandra Kukharska <aleksandra_kw@hotmail.com>
2026-01-25 15:28:46 +0100

    Access to backend folder-Marianna

--- backend/run_server.py
+++ backend/run_server.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-# Define o caminho base para o Python encontrar a pasta 'backend'
+# Define path for folder 'backend'
 sys.path.append(r"C:\Users\Aleks\Documents\Subscription-Tracker-App")
+sys.path.append(r"D:\3course\Erasmus studying\Mobile App\Full version\backend>")
 
 import uvicorn
 
 if __name__ == "__main__":
-    # O segredo aqui é o reload=False para evitar o erro de tabelas duplicadas
     uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=False)
